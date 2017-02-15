9f6eaaa553d78b01c6958a16dff447321ad35a3c
tianyingchun <tyc15129@ly.com>
2017-02-15 14:00:49 +0800

    update add angular testing

---
+++ test/es6/angular.js
@@ -0,0 +1,7 @@
+app.controller('TestCtrl', ['$scope', function ($scope) {
+  var name = 'show name';
+  var pwd = 'show password';
+  $scope.showName = function () {
+    console.log(111);
+  };
+}]);
--- test/index.js
+++ test/index.js
@@ -4,6 +4,8 @@ import { square, diag } from './es6/lib';
 
 import Circle from './es6/Circle.js';
 
+require('./es6/angular.js');
+
 var circle = new Circle();
 var area = circle.area();
 
